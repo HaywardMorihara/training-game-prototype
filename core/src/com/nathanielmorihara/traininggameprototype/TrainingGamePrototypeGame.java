c06af53fa7e54703382d51521c96aef4d533554f
HaywardMorihara <nathanielmorihara@gmail.com>
2020-07-30 22:17:16 -0400

    Load tiled map

--- core/src/com/nathanielmorihara/traininggameprototype/TrainingGamePrototypeGame.java
+++ core/src/com/nathanielmorihara/traininggameprototype/TrainingGamePrototypeGame.java
@@ -1,107 +1,95 @@
 package com.nathanielmorihara.traininggameprototype;
 
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.Box2D;
-import com.badlogic.gdx.physics.box2d.World;
-import com.nathanielmorihara.traininggameprototype.controller.PenguinController;
-import com.nathanielmorihara.traininggameprototype.controller.PlayerController;
-import com.nathanielmorihara.traininggameprototype.model.CherryModel;
-import com.nathanielmorihara.traininggameprototype.model.Model;
-import com.nathanielmorihara.traininggameprototype.model.PenguinModel;
-import com.nathanielmorihara.traininggameprototype.model.PlayerModel;
-import com.nathanielmorihara.traininggameprototype.view.CherryView;
-import com.nathanielmorihara.traininggameprototype.view.PenguinView;
-import com.nathanielmorihara.traininggameprototype.view.PlayerView;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
-public class TrainingGamePrototypeGame extends ApplicationAdapter {
-	SpriteBatch batch;
-
-	PlayerView playerView;
-	PlayerController playerController;
-
-	PenguinView penguinView;
-	PenguinController penguinController;
-
-	CherryView cherryView;
-
-	private World world; // This just holds everything and manages physics. Nothing to do with the view
-	private float time;
-
-	WorldState worldState;
+public class TrainingGamePrototypeGame extends ApplicationAdapter implements InputProcessor {
+	Texture img;
+	TiledMap tiledMap;
+	OrthographicCamera camera;
+	TiledMapRenderer tiledMapRenderer;
 
 	@Override
 	public void create () {
-		time = 0f;
-		Box2D.init();
-		world = new World(new Vector2(0, 0), true);
+		float w = Gdx.graphics.getWidth();
+		float h = Gdx.graphics.getHeight();
 
-		playerController = new PlayerController();
-		playerView = new PlayerView();
-		PlayerView.load();
-		PlayerModel playerModel = new PlayerModel(world, 1, 150, 200);
-
-		penguinController = new PenguinController();
-		penguinView = new PenguinView();
-		PenguinView.load();
-		PenguinModel penguinModel = new PenguinModel(world, 1, 400, 200);
-
-		CherryView.load();
-		cherryView = new CherryView();
-
-		worldState = new WorldState(world, playerModel, penguinModel, null);
-		world.setContactListener(new ContactListenerImpl(worldState));
-
-		batch = new SpriteBatch();
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false,w,h);
+		camera.update();
+		tiledMap = new TmxMapLoader().load("MyFirstMap.tmx");
+		tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+		Gdx.input.setInputProcessor(this);
 	}
 
 	@Override
 	public void render () {
-		time += Gdx.graphics.getDeltaTime(); // Accumulate elapsed animation time
-
-		playerController.update(worldState);
-		penguinController.update(worldState);
-
-		// TODO There's some weird concurrency stuff going on here that should probably get fixed
-		List<Body> bodiesToDestroy = new LinkedList<>(worldState.getBodiesToDestroy());
-		Iterator<Body> iterator = bodiesToDestroy.iterator();
-		while (iterator.hasNext()) {
-			Body body = iterator.next();
-			if (body.getUserData().equals("cherry")) {
-				// TODO This really should be in the contactlistener or osmething
-				penguinController.followTrainerLearner.learn(worldState);
-
-				world.destroyBody(body);
-				worldState.setCherryModel(null);
-			}
-		}
-		worldState.clearBodiesToDestroy();
-
-		world.step(1/60f, 6, 2);
-
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		playerView.draw(batch, time, worldState.getPlayerModel());
-		penguinView.draw(batch, time, worldState.getPenguinModel());
-		if (worldState.getCherryModel() != null) {
-			cherryView.draw(batch, worldState.getCherryModel());
-		}
-		batch.end();
+		camera.update();
+		tiledMapRenderer.setView(camera);
+		tiledMapRenderer.render();
 	}
-	
+
 	@Override
-	public void dispose () {
-		batch.dispose();
-		PlayerView.dispose();
-		PenguinView.dispose();
+	public boolean keyDown(int keycode) {
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		if(keycode == Input.Keys.LEFT)
+			camera.translate(-32,0);
+		if(keycode == Input.Keys.RIGHT)
+			camera.translate(32,0);
+		if(keycode == Input.Keys.UP)
+			camera.translate(0,-32);
+		if(keycode == Input.Keys.DOWN)
+			camera.translate(0,32);
+		if(keycode == Input.Keys.NUM_1)
+			tiledMap.getLayers().get(0).setVisible(!tiledMap.getLayers().get(0).isVisible());
+		if(keycode == Input.Keys.NUM_2)
+			tiledMap.getLayers().get(1).setVisible(!tiledMap.getLayers().get(1).isVisible());
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		return false;
 	}
 }
