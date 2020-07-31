85a46bdac76b0d8c11c0c4ee9e741ed8f4ba567e
HaywardMorihara <nathanielmorihara@gmail.com>
2020-07-31 09:59:03 -0400

    Combine map logic into game, but kinda broken

--- core/src/com/nathanielmorihara/traininggameprototype/TrainingGamePrototypeGame.java
+++ core/src/com/nathanielmorihara/traininggameprototype/TrainingGamePrototypeGame.java
@@ -7,11 +7,19 @@ import java.util.List;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.MapProperties;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.Box2D;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.nathanielmorihara.traininggameprototype.controller.PenguinController;
 import com.nathanielmorihara.traininggameprototype.controller.PlayerController;
 import com.nathanielmorihara.traininggameprototype.model.CherryModel;
@@ -38,8 +46,37 @@ public class TrainingGamePrototypeGame extends ApplicationAdapter {
 
 	WorldState worldState;
 
+	TiledMap tiledMap;
+	Viewport viewport;
+	OrthographicCamera camera;
+	TiledMapRenderer tiledMapRenderer;
+
+	float height;
+	float width;
+	float unitScale;
+
 	@Override
 	public void create () {
+		tiledMap = new TmxMapLoader().load("MyFirstMap.tmx");
+
+		MapProperties mapProperties = tiledMap.getProperties();
+		Integer imageheight = (Integer) mapProperties.get("height");
+		Integer imagewidth = (Integer) mapProperties.get("width");
+		Integer tileheight = (Integer) mapProperties.get("tileheight");
+		Integer tilewidth = (Integer) mapProperties.get("tilewidth");
+
+		// TODO assert the tilewidth and tileheight are the same
+
+		height = (float) imageheight;
+		width = (float) imagewidth;
+		unitScale = (float) 1 / tileheight;
+
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, imagewidth, imageheight);
+		viewport = new FitViewport(imagewidth, imageheight, camera);
+
+		tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+
 		time = 0f;
 		Box2D.init();
 		world = new World(new Vector2(0, 0), true);
@@ -89,6 +126,12 @@ public class TrainingGamePrototypeGame extends ApplicationAdapter {
 
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+//		camera.translate(1,1); // Move the camera
+		camera.update();
+		tiledMapRenderer.setView(camera);
+		tiledMapRenderer.render();
+
 		batch.begin();
 		playerView.draw(batch, time, worldState.getPlayerModel());
 		penguinView.draw(batch, time, worldState.getPenguinModel());
